077728ae9285410d0e6b95c4882b6802195d962c
lizhen <lizhen@ainirobot.com>
2018-08-24 15:33:32 +0800

    Second submission

--- mystring.cc
+++ mystring.cc
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
   cout << "hello~" << endl;
-  test03();
+  test04();
 }
 
 void test01() {
@@ -30,4 +30,13 @@ void test03() {
   // delete p_reg;
   pd.reset();
   cout << "pd: " << pd.get() << endl;
+}
+
+void test04() {
+  const int N = 6;
+  string s1[N] = {"buffoon", "thinkers", "for", "heavy", "can", "for"};
+  set<string> A(s1, s1 + N);
+  ostream_iterator<string, char> out(cout, " ");
+  copy(A.begin(), A.end(), out);
+  cout << endl;
 }
--- mystring.h
+++ mystring.h
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <set>
+#include <iterator>
 
 void test01();
 
 void test02();
 
 void test03();
+
+void test04();
